2fae5feecb1046d83639269a636be4e213d16bb8
Mohamed Ali <113967204+modev369@users.noreply.github.com>
2023-05-10 05:46:45 +0300

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 i = 0
-for c in range(ord('z', ord('a') - 1, -1):
+for c in range(ord('z', ord('a') - 1, -1)
         print("{}".format(chr(c - i)), end="")
-        i = 32 if i == 0 else 0
+       
+               if (i == 0):
+               i = 32
+               else:
+               i = 0
