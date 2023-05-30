e1a75938d3c0a7003ea3ee9631ece15232b6043b
modev369 <mo.alidev369@gmail.com>
2023-05-29 19:51:26 -0700

    modev369

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,8 +18,6 @@ def list_division(my_list_1, my_list_2, list_length):
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
 
-            #the 5th task#
-
             print("wrong type")
             div = 0
         except ZeroDivisionError:
